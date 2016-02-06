be03b9442ac0add0e11513c4f21eecfbd6575e52
Varsha Vijayvargiya <varsha@okutech.in>
2016-02-06 11:23:44 +0530

    Initial Commit

---
+++ RoposoTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RoposoTest
+//
+//  Created by Okutech on 2/6/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RoposoTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RoposoTest
+//
+//  Created by Okutech on 2/6/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
