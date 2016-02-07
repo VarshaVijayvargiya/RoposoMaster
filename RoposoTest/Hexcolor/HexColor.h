160aeb69e43db99c025e853bcf6ce06ebc1ad126
Varsha Vijayvargiya <varsha@okutech.in>
2016-02-07 08:32:57 +0530

    hex model classes

---
+++ RoposoTest/Hexcolor/HexColor.h
@@ -0,0 +1,15 @@
+//
+//  HexColor.h
+
+
+
+
+#import <UIKit/UIKit.h>
+
+@interface HexColor : UIViewController
+
+
++(UIColor*)colorWithHexString:(NSString*)hex;
++ (UIColor *)setbuttonColor;
+
+@end
